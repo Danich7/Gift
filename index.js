05a004cd050404e32410fda5bdb3453a58d5eab6
Danich7 <Danich132@yandex.ru>
2021-12-08 18:25:30 +0300

    3 commit

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function Kwa () {
-  let audio = new Audio();
-  audio.src = "https://danich7.github.io/Gift/audio/frog.mp3";
-  document.body.append(audio);
+  const audio = document.querySelector(".audio");
   audio.play();
 };
